c0291a83663fd264bbbbdc989f528bcc5937474a
med-amiine <Med.amine.idmoussi@gmail.com>
2025-09-18 19:35:56 +0300

    feat: initialize NEAR smart contract project structure
    - Create yetify-contracts directory
    - Add basic Cargo.toml with NEAR SDK dependency
    - Initialize contract struct with near_bindgen
    - Set up foundation for strategy storage contract

---
+++ yetify-contracts/strategy-storage/src/lib.rs
@@ -0,0 +1,12 @@
+use near_sdk::near_bindgen;
+
+#[near_bindgen]
+#[derive(Default)]
+pub struct YetifyStrategyStorage {
+    // Contract state will be added incrementally
+}
+
+#[near_bindgen]
+impl YetifyStrategyStorage {
+    // Functions will be added step by step
+}
