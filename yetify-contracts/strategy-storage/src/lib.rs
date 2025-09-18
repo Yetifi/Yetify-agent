c1760d8dbd7de707491afc03eefab9e53a258adf
med-amiine <Med.amine.idmoussi@gmail.com>
2025-09-18 19:46:00 +0300

    feat: add strategy data struct definition

--- yetify-contracts/strategy-storage/src/lib.rs
+++ yetify-contracts/strategy-storage/src/lib.rs
@@ -1,4 +1,17 @@
-use near_sdk::near_bindgen;
+use near_sdk::{near_bindgen, AccountId};
+use serde::{Deserialize, Serialize};
+
+#[derive(Serialize, Deserialize, Clone)]
+#[serde(crate = "near_sdk::serde")]
+pub struct StrategyData {
+    pub id: String,
+    pub goal: String,
+    pub chains: Vec<String>,
+    pub protocols: Vec<String>,
+    pub risk_level: String,
+    pub creator: AccountId,
+    pub created_at: u64,
+}
 
 #[near_bindgen]
 #[derive(Default)]
